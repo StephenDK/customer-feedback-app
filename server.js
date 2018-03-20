19a7e6ac2f2f1a53a0e8a09b704b8dc1b529032a
StephenDK <stephendklein08@gmail.com>
2018-03-19 21:45:15 -0700

    New passport instance

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 // Import express library
 const express = require('express');
+
+// Import passport *Passport step 1*
+const passport = require('passport');
+// Google passport Strategy *Passport step 2*
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// New passport instance *Passport step 3*
+passport.use(new GoogleStrategy());
+
+
 // express instance
 const app = express();
+
 // PORT
 const PORT = process.env.PORT || 5000;
 
-// Home route handler
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+
 
 
 
