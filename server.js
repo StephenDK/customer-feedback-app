4a15c277c8c734bbf0da54fb01af5ea3fb42c066
StephenDK <stephendklein08@gmail.com>
2018-03-19 22:16:37 -0700

    GoogleStrategy Setup

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const express = require('express');
 const passport = require('passport');
 // Import Google passport Strategy *Passport step 2*
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// Import Google+ API keys *Passport step 4*
+const keys = require('./config/keys');
 
 // New passport instance *Passport step 3*
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        // Callback URL
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+);
 
 
 // express instance
