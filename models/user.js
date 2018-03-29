f7bd0d4e09a8d3efdde3ffdd7d82c657fa436c93
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-03-28 17:29:50 -0700

    User schema

--- models/user.js
+++ models/user.js
@@ -4,3 +4,9 @@ const mongoose = require('mongoose');
 // mongoose schema object
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+    googleId: String
+});
+
+// Tell mongoose to create a new collection
+mongoose.model('users', userSchema);
