a30868835ba0b21651fa926904a4d61a4893f7cb
StephenDK <stephendklein08@gmail.com>
2018-04-15 17:45:50 -0700

    react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import react router dom
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
     return (
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import reducers
+import reducers from './reducers';
 
 import App from './components/App';
+// redux store 
+const store = createStore(reducers, {}, applyMiddleware());
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// wire up app with the provider tag from react-redux
+ReactDOM.render(
+<Provider store={store}><App /></Provider>,
+ document.getElementById("root")
+);
