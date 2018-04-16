7416fd526caa9506d93fa90eb4c8ab17b1d59824
StephenDK <stephendklein08@gmail.com>
2018-04-16 15:22:08 -0700

    user state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,40 @@
+// Import class component from react
 import React, { Component } from 'react';
+// import the connect helper from react-redux
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>
+            default:
+                return <li><a href="">Logout</a></li>
+        }
+
+    }
+    
+    
     render() {
+        console.log(this.props);
         return (
             <nav>
                 <div className='nav-wrapper'>
                     <a className='left brand-logo'>CustomerServer</a>
                     <ul className='right'>
-                        <li>
-                            <a>Login with Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
         );
     }
 }
+// calls the entire state object out of the redux store
+function mapStateToProps({ auth }) {
+    return { auth };
+}
 
-export default Header;
+// export navbar
+export default connect(mapStateToProps) (Header);
