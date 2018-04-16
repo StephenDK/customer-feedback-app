9a03d936efa64740ac5391d31cd7355aab0eef45
StephenDK <stephendklein08@gmail.com>
2018-04-15 21:13:45 -0700

    react router exact paths

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,24 @@ import React from 'react';
 // import react router dom
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
 const App = () => {
     return (
         <div>
-            Hi there!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                     {/* Use the exact key word or exact={true} to keep components from 
+                    displaying on the same page */}
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/dashboard" component={Dashboard} />
+                    <Route path="/dashboard/newsurvey/" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
