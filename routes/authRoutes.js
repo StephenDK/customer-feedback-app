f1474a14d4156723e982ea4d15f17d79db85453d
StephenDK <stephendklein08@gmail.com>
2018-04-26 00:12:00 -0700

    heroku test 3

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,7 +21,7 @@ module.exports = (app) => {
     app.get('/auth/google/callback', 
     passport.authenticate('google'),
     (req, res) => {
-        res.redirect('https://quiet-badlands-38905.herokuapp.com/dashboard');
+        res.redirect('/dashboard');
     });
 
     // If the current user is signed into the application
