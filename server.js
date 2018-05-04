5bee6309c849ad27f75c38c17e29529803eeb205
StephenDK <stephendklein08@gmail.com>
2018-05-04 14:49:35 -0700

    production routes

--- server.js
+++ server.js
@@ -47,6 +47,20 @@ require('./routes/authRoutes')(app);
 // billing routes
 require('./routes/billingRoutes')(app);
 
+// when the application is in production
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve production assests
+    // like main.js file, or main.css file
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesen't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 // PORT
 const PORT = process.env.PORT || 5000;
 
