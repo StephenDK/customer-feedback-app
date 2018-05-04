1b95ada30eb52f6e55f5ae175774568bccedda1d
StephenDK <stephendklein08@gmail.com>
2018-05-04 13:31:23 -0700

    middleware

---
+++ middlewares/requireLogin.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    // takes the income request and modifies the body
+    // next argument runs after the function is complete ie done() in passport
+    if (!req.user) {
+        return res.status(401).send({ error: 'You must log in!' });
+    }
+
+    next();
+};
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,17 +1,12 @@
-
 // require keys
 const keys = require('../config/keys');
 // require stripe
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
 
-    app.post('/api/stripe', async (req, res) => {
-        if (!req.user) {
-            return res.status(401).send({ error: "You must log in!" })
-        }
-        
-        
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
