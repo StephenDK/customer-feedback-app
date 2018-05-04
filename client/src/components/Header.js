d1e3d6db94a1f5abc6422dc60aab8422f39eb2e2
StephenDK <stephendklein08@gmail.com>
2018-05-03 18:22:27 -0700

    stripe checkout and styles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,15 +4,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+// import payments
+import Payments from './Payments';
+
 class Header extends Component {
     renderContent() {
         switch (this.props.auth) {
             case null:
                 return;
             case false:
-                return <li><a href="/auth/google">Login with Google</a></li>
+                return <li><a href="/auth/google">Login with Google</a></li>;
             default:
-                return <li><a href="/api/logout">Logout</a></li>
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
 
     }
---
+++ client/src/components/Payments.js
@@ -0,0 +1,25 @@
+// import class component from react
+import React, { Component } from 'react';
+
+// import react-stripe-checkout
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+
+    render() {
+        return(
+            <StripeCheckout
+            name="CustomerServer"
+            description="$5 for 5 email credits." 
+            amount={500}
+            token={token => console.log(token)}
+            stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            >
+            <button className="waves-effect waves-light btn">
+            Add Credits
+            </button>
+            </StripeCheckout>
+        );
+    }
+}
+export default Payments;
