48161134e8b34afc9c02034c76c0f3d5a70dc7f6
StephenDK <stephendklein08@gmail.com>
2018-05-08 14:49:27 -0700

    ref survey model

--- models/Survey.js
+++ models/Survey.js
@@ -11,7 +11,13 @@ const surveySchema = new Schema({
     subject: String,
     recipients: [RecipientSchema],
     yes: { type: Number, default: 0 },
-    no: { type: Number, default: 0 }
+    no: { type: Number, default: 0 },
+    // the type is the id of the user that owns the record
+    // The ref tells mongoose to get the objectId from the User collection
+    // the underscore indicates that this is a reference field
+    _user: { type: Schema.Types.ObjectId, ref: 'User' },
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
