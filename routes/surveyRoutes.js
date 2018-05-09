7636ba06b4bab9cabc99205be87116a9c1067102
StephenDK <stephendklein08@gmail.com>
2018-05-08 17:33:03 -0700

    new aurvey instance

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,10 @@
+const mongoose = require('mongoose');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
 
