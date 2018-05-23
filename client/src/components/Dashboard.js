ad16efe5dff4661a9a6cea903bf1ab49b4e13b9c
StephenDK <stephendklein08@gmail.com>
2018-05-23 16:31:28 -0700

    materialize icons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,6 +4,11 @@ const Dashboard = () => {
     return (
       <div>
         Dashboard
+        <div className='fixed-action-btn'>
+            <a className='btn-floating btn-large green'>
+                <i className='material-icons'>add</i>
+            </a>
+        </div>
       </div>
     )
   
