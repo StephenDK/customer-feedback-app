c22e866f60f7a638da4e80a30d344743449e8944
StephenDK <stephendklein08@gmail.com>
2018-05-24 09:45:43 -0700

    redux-form component

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,23 @@
+// =========================================
+// REDUX FORM SETUP SET 3:
+// Create a react class component 
+
+// REDUX FORM SETUP SET 4:
+// Add SurveyNew component to App component
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+
+    render() {
+        return (
+            <div>
+                SurveyNew!
+            </div>
+        );
+    }
+}
+
+export default SurveyNew;
+
+// =========================================
+
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,20 @@
 import { combineReducers } from 'redux';
+
+// ==========================================
+// REDUX FORM SETUP SET 1:
+// Import the form reducer function from redux-form.
+// Use as to rename the reducer function to reduxForm.
+import { reducer as reduxForm } from 'redux-form';
+// ==========================================
+
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    // ========================================
+    // REDUX FORM SETUP SET 2:
+    // Add reduxForm reducer to combineReducers.
+    // Use form key to refer to reduxForm reducer
+    form: reduxForm
+    // ========================================
 });
