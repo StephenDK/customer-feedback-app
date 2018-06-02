513fba64cab75513b62edd8a309b0d99f96c0065
StephenDK <stephendklein08@gmail.com>
2018-06-01 22:54:47 -0700

    Advancing from SurveyForm

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -47,7 +47,7 @@ class SurveyForm extends Component {
     return (
         <div>
           <form 
-          onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
             {this.renderFields()}
             <Link to='/dashboard' className='red btn-flat white-text'>
             Cancel
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -8,16 +8,38 @@
 
 // REDUX FORM SETUP STEP 6:
 // Import the SurveyForm component
-import SurveyForm from './SurveyForm';
-
 import React, { Component } from 'react';
 
+import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
+
+
 class SurveyNew extends Component {
+// =========================================
+// REDUX FORM SETUP STEP 17:
+// set state in this component to toggle wether or not we show the form review
+state = { showFormReview: false };
+
+// This helper function will determine if we show form review
+renderContent() {
+    if (this.state.showFormReview) {
+        return <SurveyFormReview />
+    }
+    // in show form review we are going to pass a callback function
+    // in the props that will set the state to true when is is called
+    return <SurveyForm 
+    onSurveySubmit={() => this.setState({ showFormReview: true })}
+    // now in the survey foem we will call the prop onSurveySubmit function in 
+    // the onSubmit event handler 
+    />
+}
+
+// =========================================
 
     render() {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
