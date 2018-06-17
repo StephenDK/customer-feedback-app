5278e0e83fa467ce00f6ffd8840230f6bf2edadd
StephenDK <stephendklein08@gmail.com>
2018-06-17 16:55:56 -0700

    Unique events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -56,11 +56,24 @@ module.exports = app => {
     app.post('/api/surveys/webhooks', (req, res) => {
         // console.log(req.body); List of events
         // res.send({});
-        const events = _.map(req.body, (event) => {
-            const pathname = new URL(event.url).pathname;
+        const events = _.map(req.body, ({ email, url }) => {
+            const pathname = new URL(url).pathname;
             const p = new Path('/api/surveys/:surveyId/:choice');
-            console.log(p.test(pathname));
-        })
+            // console.log(p.test(pathname));
+            const match = p.test(pathname);
+
+            if (match) {
+                return { email, surveyId: match.surveyId, choice: match.choice};
+            }
+        });
+
+        // console.log(events); returns an array of elements
+
+        const compactEvents = _.compact(events);
+        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+        console.log(uniqueEvents);
+        res.send({});
     });
 
 };
