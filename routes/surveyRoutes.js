40e8828a84d7bf54fa615c78be99a2a142d5151a
StephenDK <stephendklein08@gmail.com>
2018-06-17 18:37:54 -0700

    Lodash chain helper

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -56,23 +56,24 @@ module.exports = app => {
     app.post('/api/surveys/webhooks', (req, res) => {
         // console.log(req.body); List of events
         // res.send({});
-        const events = _.map(req.body, ({ email, url }) => {
-            const pathname = new URL(url).pathname;
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            // console.log(p.test(pathname));
-            const match = p.test(pathname);
+        const p = new Path('/api/surveys/:surveyId/:choice');
 
-            if (match) {
-                return { email, surveyId: match.surveyId, choice: match.choice};
-            }
-        });
+        const events = _.chain(req.body)
+            .map(({ email, url }) => {
+            
+                // console.log(p.test(pathname));
+                const match = p.test(new URL(url).pathname);
 
-        // console.log(events); returns an array of elements
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice};
+                }
+            })
+    // console.log(events); returns an array of elements
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .value();
 
-        const compactEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-
-        console.log(uniqueEvents);
+        console.log(events);
         res.send({});
     });
 
